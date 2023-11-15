31eb9dc03e22d3e365bcbc6deb3a5db68cd965b4
yuleeee <43906775+yuleeee@users.noreply.github.com>
2023-11-15 21:08:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const rssParser = new Parser();
 async function start() {
     // const blogResp = await rssParser.parseURL('https://blog.cmyr.ltd/atom.xml');
     if (da) {
-        console.log('更新博客链接成功');
-        return;
+        // console.log('更新博客链接成功');
+        // return;
     }
     // const items = [...blogResp.items.slice(0, 5)];
     // const text = items.map((e) => `- [${e.title}](${e.link})`).join('\n');
-    // const readme = await fs.readFile('README.md', 'utf-8');
-    // const newReadme = readme.replace(/<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->/, `<!-- BLOG_START -->\n${text}\n<!-- BLOG_END -->`);
-    // await fs.writeFile('README.md', newReadme);
-    // console.log('更新博客链接成功');
+    const readme = await fs.readFile('README.md', 'utf-8');
+    const newReadme = readme.replace(/<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->/, `<!-- BLOG_START -->\n${da}\n<!-- BLOG_END -->`);
+    await fs.writeFile('README.md', newReadme);
+    console.log('更新博客链接成功');
 }
 start();
