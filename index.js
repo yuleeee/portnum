92a3e8fd8f74b3e742dbc8e1ef64b5212d001cb7
yuleeee <43906775+yuleeee@users.noreply.github.com>
2023-11-15 20:59:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,19 @@
 
 var fs = require('fs-extra');
 var Parser = require('rss-parser');
-
+var da = Math.floor(Date.now() - new Date(2023, 10, 2, 8, 0, 0, 0));
 const rssParser = new Parser();
 async function start() {
-    const blogResp = await rssParser.parseURL('https://blog.cmyr.ltd/atom.xml');
-    if (Date.now() - new Date(blogResp.lastBuildDate).getTime() > 48 * 60 * 60 * 1000) {
-        console.log('最近48小时内没有博客更新，跳过本次更新');
+    // const blogResp = await rssParser.parseURL('https://blog.cmyr.ltd/atom.xml');
+    if (da) {
+        console.log('更新博客链接成功');
         return;
     }
-    const items = [...blogResp.items.slice(0, 5)];
-    const text = items.map((e) => `- [${e.title}](${e.link})`).join('\n');
-    const readme = await fs.readFile('README.md', 'utf-8');
-    const newReadme = readme.replace(/<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->/, `<!-- BLOG_START -->\n${text}\n<!-- BLOG_END -->`);
-    await fs.writeFile('README.md', newReadme);
-    console.log('更新博客链接成功');
+    // const items = [...blogResp.items.slice(0, 5)];
+    // const text = items.map((e) => `- [${e.title}](${e.link})`).join('\n');
+    // const readme = await fs.readFile('README.md', 'utf-8');
+    // const newReadme = readme.replace(/<!-- BLOG_START -->([\s\S]*?)<!-- BLOG_END -->/, `<!-- BLOG_START -->\n${text}\n<!-- BLOG_END -->`);
+    // await fs.writeFile('README.md', newReadme);
+    // console.log('更新博客链接成功');
 }
 start();
